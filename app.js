000ce577bbf5ad6a624fd34405860e043d195ced
Sean Thomas Anderson <Anderson.Sean27@gmail.com>
2016-06-20 19:04:48 -0700

    Capitol Hill

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var hours = ['6:00am', '7:00am','8:00am','9:00am','10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
-
 var firstAndPike = {
   minimumCustomers: 23,
   maximumCustomers: 65,
@@ -28,7 +27,6 @@ for (var i = 0; i < hours.length; i++) {
   var listContent = hours[i] + ' ' + firstAndPike.hourlyCustomersArray[i];
   listItem.textContent = listContent;
   customerList.appendChild(listItem);}
-
 var customerList2 = document.getElementById('firstAndPike_Purchases');
 for (var i = 0; i < hours.length; i++) {
   var listItem = document.createElement('li');
@@ -36,7 +34,6 @@ for (var i = 0; i < hours.length; i++) {
   listItem.textContent = listContent;
   customerList2.appendChild(listItem);
 }
-
 var seaTacAirport = {
   minimumCustomers: 3,
   maximumCustomers: 24,
@@ -48,7 +45,6 @@ var seaTacAirport = {
       this.hourlyCustomersArray.push(customers);
     }
   },
-
   hourlyPurchases: function() {
     for (var i = 0; i < hours.length; i++) {
       var purchases = Math.floor(Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers) + this.minimumCustomers) * this.averageCookies);
@@ -67,7 +63,6 @@ for (var i = 0; i < hours.length; i++) {
   var listContent = hours[i] + ' ' + seaTacAirport.hourlyCustomersArray[i];
   listItem.textContent = listContent;
   customerList.appendChild(listItem);}
-
 var customerList2 = document.getElementById('seaTacAirport_Purchases');
 for (var i = 0; i < hours.length; i++) {
   var listItem = document.createElement('li');
@@ -104,7 +99,6 @@ for (var i = 0; i < hours.length; i++) {
   var listContent = hours[i] + ' ' + seattleCenter.hourlyCustomersArray[i];
   listItem.textContent = listContent;
   customerList.appendChild(listItem);}
-
 var customerList2 = document.getElementById('seattleCenter_Purchases');
 for (var i = 0; i < hours.length; i++) {
   var listItem = document.createElement('li');
@@ -119,16 +113,34 @@ var capitolHill = {
   hourlyCustomers: function() {
     for (var i = 0; i < hours.length; i++) {
       var customers = Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers) + this.minimumCustomers);
-      console.log(hours[i], customers);
+      this.hourlyCustomersArray.push(customers);
     }
   },
   hourlyPurchases: function() {
     for (var i = 0; i < hours.length; i++) {
       var purchases = Math.floor(Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers) + this.minimumCustomers) * this.averageCookies);
       console.log(hours[i], purchases);
+      this.hourlyPurchasesArray.push(purchases);
     }
-  }
+  },
+  hourlyCustomersArray : [],
+  hourlyPurchasesArray : []
 };
+capitolHill.hourlyCustomers();
+capitolHill.hourlyPurchases();
+var customerList =  document.getElementById('capitolHill_Customers');
+for (var i = 0; i < hours.length; i++) {
+  var listItem = document.createElement('li');
+  var listContent = hours[i] + ' ' + capitolHill.hourlyCustomersArray[i];
+  listItem.textContent = listContent;
+  customerList.appendChild(listItem);}
+var customerList2 = document.getElementById('capitolHill_Purchases');
+for (var i = 0; i < hours.length; i++) {
+  var listItem = document.createElement('li');
+  var listContent = hours[i] + ' ' + capitolHill.hourlyPurchasesArray[i];
+  listItem.textContent = listContent;
+  customerList2.appendChild(listItem);
+}
 var alki = {
   minimumCustomers: 2,
   maximumCustomers: 16,
