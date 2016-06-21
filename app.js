63b57130a3626b9810be31e9434560bd9f14c6e1
Sean Thomas Anderson <Anderson.Sean27@gmail.com>
2016-06-21 16:53:53 -0700

    comments

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-'use strict';
-var hours = ['6:00am', '7:00am','8:00am','9:00am','10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
-var cookieTable = document.getElementById('CookieSales');
+//sets parameters on the functionality of javascript.'use strict';
+// declares an array containing the hours of the day.var hours = ['6:00am', '7:00am','8:00am','9:00am','10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
+//declares a variable called cookie table, links to htmlvar cookieTable = document.getElementById('CookieSales');
 
-function makeHeaderRow () {
-  var headerRow = document.createElement('tr');
-  var headerCell = document.createElement('th');
-  headerCell.textContent = '';
-  headerRow.appendChild(headerCell);
-  var headerCell = document.createElement('th');
-  headerCell.textContent = 'Daily Location Total';
-  headerRow.appendChild(headerCell);
-  for(var i = 0; i < hours.length; i++) {
-    var headerCell = document.createElement('th');
-    headerCell.textContent = hours[i];
-    headerRow.appendChild(headerCell);
+//function that makes header rowfunction makeHeaderRow () {
+//creates table row  var headerRow = document.createElement('tr');
+//  var headerCell = document.createElement('th');
+// puts a space in the header cell  headerCell.textContent = '';
+// appends header cell to header row  headerRow.appendChild(headerCell);
+//declares header cell variable, creates table head  var headerCell = document.createElement('th');
+// fills header cell with DLT  headerCell.textContent = 'Daily Location Total';
+//appends header cell to header row  headerRow.appendChild(headerCell);
+// loops through hours array  for(var i = 0; i < hours.length; i++) {
+//  creates a var named header cell and assigns a created element named th var headerCell = document.createElement('th');
+// loops through hours array and logs that data to headerCell    headerCell.textContent = hours[i];
+// appends header cell to header row    headerRow.appendChild(headerCell);
   }
-  cookieTable.appendChild(headerRow);
+//apppends header row to cookie table  cookieTable.appendChild(headerRow);
 }
 
-makeHeaderRow();
+// invokes make header row function makeHeaderRow();
 
-function CookieStand (storeLocation, minimumCustomers, maximumCustomers, averageCookies, totalDailyCookieSales) {
-  this.storeLocation = storeLocation;
-  this.hourlyCustomersArray = [];
-  this.hourlyPurchasesArray = [];
-  this.minimumCustomers = minimumCustomers;
-  this.maximumCustomers = maximumCustomers;
-  this.averageCookies = averageCookies;
-  this.totalDailyCookieSales = totalDailyCookieSales;
-  this.getHourlyCustomers = function () {
-    for (var i = 0; i < hours.length; i++) {
-      var customers = Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers) + this.minimumCustomers);
-      this.hourlyCustomersArray.push(customers);
+// constructor function CookieStand (storeLocation, minimumCustomers, maximumCustomers, averageCookies, totalDailyCookieSales) {
+//property  this.storeLocation = storeLocation;
+//creates array shell for hourly customers  this.hourlyCustomersArray = [];
+//creates array shell for hourly purchases  this.hourlyPurchasesArray = [];
+//property  this.minimumCustomers = minimumCustomers;
+//property  this.maximumCustomers = maximumCustomers;
+//property  this.averageCookies = averageCookies;
+  // propertythis.totalDailyCookieSales = totalDailyCookieSales;
+//method that finds hourly customers   this.getHourlyCustomers = function () {
+//loops through hours    for (var i = 0; i < hours.length; i++) {
+    // method  var customers = Math.floor(Math.random() * (this.maximumCustomers - this.minimumCustomers) + this.minimumCustomers);
+  //pushes customers    this.hourlyCustomersArray.push(customers);
     }
   };
   this.getHourlyPurchases = function () {
